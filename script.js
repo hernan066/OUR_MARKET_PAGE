362c82c2fdbe05ea24b164a288a30b87d5eff9be
hernan066 <hernan0066@gmail.com>
2023-09-27 17:10:18 -0300

    Fix mobile and margins

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const nav = document.querySelector(".nav");
+const send_email = document.getElementById("send_email");
+const msg = document.querySelector(".msg");
+const MENSAJE = "Mensaje recibido, nos contactaremos en breve!!";
+
 window.addEventListener("scroll", fixNav);
+
+send_email.addEventListener("click", (e) => {
+  msg.innerHTML = MENSAJE;
+
+  setTimeout(() => {
+    msg.innerHTML = "";
+  }, "5000");
+});
 
 function fixNav() {
   if (window.scrollY > nav.offsetHeight + 150) {
