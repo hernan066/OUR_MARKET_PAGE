dc0b8cf5b947c35d9c5d48b7c26f308cd3e0cd30
hernan066 <hernan0066@gmail.com>
2023-09-27 18:05:55 -0300

    Fix menu mobile

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const nav = document.querySelector(".nav");
 const menu = document.querySelector(".links__menu-mobile");
 const menu_toggle = document.querySelector(".menu-toggle");
+const myCheckbox = document.querySelector(".myCheckbox");
+const links_menu = document.querySelectorAll(".menu-mobile");
 const send_email = document.getElementById("send_email");
+
 const msg = document.querySelector(".msg");
 const MENSAJE = "Mensaje recibido, nos contactaremos en breve!!";
 
 window.addEventListener("scroll", fixNav);
+
+links_menu.forEach((item) => {
+  item.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    myCheckbox.checked = false;
+  });
+});
 
 menu_toggle.addEventListener("click", (e) => {
   menu.classList.toggle("active");
