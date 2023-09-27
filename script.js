8e5d920bf0f837cff8dad5a6eb2ba5775ca5f7d2
hernan066 <hernan0066@gmail.com>
2023-09-27 17:49:03 -0300

    Add menu mobile

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const nav = document.querySelector(".nav");
+const menu = document.querySelector(".links__menu-mobile");
+const menu_toggle = document.querySelector(".menu-toggle");
 const send_email = document.getElementById("send_email");
 const msg = document.querySelector(".msg");
 const MENSAJE = "Mensaje recibido, nos contactaremos en breve!!";
 
 window.addEventListener("scroll", fixNav);
+
+menu_toggle.addEventListener("click", (e) => {
+  menu.classList.toggle("active");
+});
 
 send_email.addEventListener("click", (e) => {
   msg.innerHTML = MENSAJE;
